00b52a552718bf1302321e5f1b7f44dcda4c3aca
harshit rastogi <harshit@gogii.net>
2012-10-26 01:45:47 -0700

    set property

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
       :address              => "smtp.gmail.com",  
       :port                 => 587,  
       :domain               => "gmail.com",  
-     :user_name            => "hrsht.rastogi@gmail.com", #Your user name
-      :password             => "monica02", # Your password
+     :user_name            => ENV["GMAIL_USERNAME"],
+      :password             => ENV["GMAIL_PASSWORD"],
       :authentication       => "plain",  
       :enable_starttls_auto => true  
    }
