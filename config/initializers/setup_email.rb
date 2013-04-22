8584527adaada51a48cac7724f0e164b84471866
harshit rastogi <harshit@gogii.net>
2013-04-22 00:15:53 -0700

    fix

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
       :address              => "smtpout.secureserver.net",  
       :port                 => 80,  
       :domain               => "www.umvox.com",  
-     :user_name            => "support@umvox.com",
-      :password             => "Complaints1",
+     :user_name            => ENV["GMAIL_USERNAME"],
+      :password             => ENV["GMAIL_PASSWORD"],
       :authentication       => "plain",  
       :enable_starttls_auto => true  
    }
