cdf20454594834eea387de951869eb581992f459
harshit rastogi <harshit@gogii.net>
2013-04-29 03:41:00 -0700

    fix

--- db/migrate/20130429061106_add_column_to_user.rb
+++ db/migrate/20130429061106_add_column_to_user.rb
@@ -1,6 +1,6 @@
 class AddColumnToUser < ActiveRecord::Migration
   def up
-    add_column :users, :subscribe, :boolean, :default=>1
+    add_column :users, :subscribe, :boolean, :default=>:true
     add_column :users, :subscribe_token, :string
   end
 end
