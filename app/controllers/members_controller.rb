17552f86238cb9fbbc2598b66a0d9f1f38e77dd6
harshit rastogi <harshit@gogii.net>
2013-05-05 19:28:49 -0700

    fix

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -133,6 +133,7 @@ class MembersController < ApplicationController
      @user.country = params[:user][:country]
      @user.fbid = params[:user][:fbid]
      @user.subscribe = params[:user][:subscribe]
+     @user.admin = params[:user][:admin]
      if @user.subscribe_token.nil?
        puts "generated token"
        @user.subscribe_token = loop do
