6df5fc564b5f53f7515824ef58502583692a1457
Harsh <hrsht.rastogi13@gmail.com>
2014-07-13 18:53:29 -0700

    refactored code

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,21 +1,18 @@
 class MembersController < ApplicationController
 
-#   before_filter :authenticate1_user , :only =>[:dashboard]
+  #   before_filter :authenticate1_user , :only =>[:dashboard]
 
   def filldetails
     
     if session['access_token']
       @graph = Koala::Facebook::API.new(session["access_token"])
       userinfo =  @graph.get_object("me")
-      @user = User.new
-      @user.firstname = userinfo['first_name']
-      @user.lastname = userinfo['last_name']
-      @user.username = userinfo['username']
+      city =""
       unless userinfo['location'] == nil
-        @user.city = userinfo['location']['name']
-      end  
-      @user.fbid=userinfo['id']
-      @user.email = userinfo['email']
+        city = userinfo['location']['name']
+      end 
+      @user = User.new(:firstname=>userinfo['first_name'], :lastname=>userinfo['last_name'],
+      :username=>userinfo['username'], :fbid=>userinfo['id'],:email=>userinfo['email'],:city=>city)
     else
       @user = current_user
     end
@@ -42,29 +39,24 @@ class MembersController < ApplicationController
       @graph = Koala::Facebook::API.new(session["access_token"])
       userinfo =  @graph.get_object("me")
       fb_id=userinfo['id']
-      if fb_id
-       userObj = User.find_by_fbid(fb_id.to_s) 
-        if userObj.nil?
-          redirect_to '/members/filldetails'
-        else
-           session[:user] = userObj
-           current_user= userObj
-           redirect_to "/members/dashboard"
-        end
+      userObj = User.find_by_fbid(fb_id.to_s) 
+      if userObj.nil?
+        redirect_to '/members/filldetails'
+      else
+        session[:user] = userObj
+        current_user= userObj
+        redirect_to "/members/dashboard"
       end
     elsif !current_user.nil?
-      puts "inside2"
       if session[:new_user] != true
         session[:user] = current_user
         redirect_to "/members/dashboard"
       else
-#        @user = current_user
         redirect_to '/members/filldetails'
       end
     else  
       #    this is logout. its a temp solution
-    redirect_to '/welcome'
-
+      redirect_to '/welcome'
     end
   end
   
@@ -79,59 +71,41 @@ class MembersController < ApplicationController
     else
       @totalclose = Post.latest_closed
       @totalopen = Post.latest_open
-  end
     end
+  end
       
   
   def show
-    @user= User.find_by_id(params[:id])
+    @user= User.find(params[:id])
     session[:user] = @user
-      openUmvox=@user.posts.where("status=?","Open")
-      closeUmvox=@user.posts.where("status=?","Closed")
-      @totalopen = openUmvox.size unless openUmvox.nil?
-      @totalclosed = closeUmvox.length unless closeUmvox.nil?
-#    end
+    openUmvox=@user.posts.where("status=?","Open")
+    closeUmvox=@user.posts.where("status=?","Closed")
+    @totalopen = openUmvox.size unless openUmvox.nil?
+    @totalclosed = closeUmvox.length unless closeUmvox.nil?
+    #    end
   end
   
   def updateprofile
-    if  ! params[:user][:id].blank?
-       @user = User.find(params[:user][:id])
-       if @user.nil?
-         @user = User.new
-         @user.password ="qaumxvo"
-       end
-    else
-      @user = User.new
-#      not the best way
-      @user.password ="qaumxvo" 
-    end  
-    @user.firstname=params[:user][:firstname]
-     @user.username = params[:user][:username]
-     @user.lastname = params[:user][:lastname]
-     @user.email = params[:user][:email]
-     @user.city = params[:user][:city]
-     @user.state = params[:user][:state]
-     @user.country = params[:user][:country]
-     @user.fbid = params[:user][:fbid]
-     @user.subscribe = params[:user][:subscribe]
-     if @user.subscribe_token.nil?
-       puts "generated token"
-       @user.subscribe_token = loop do
+    @user = User.find(params[:user][:id])
+    @user.update_attributes(params[:user])
+    if @user.subscribe_token.nil?
+      puts "generated token"
+      @user.subscribe_token = loop do
         random_token = SecureRandom.urlsafe_base64
-      break random_token unless User.where(subscribe_token: random_token).exists?
-       end  
+        break random_token unless User.where(subscribe_token: random_token).exists?
+      end  
     end
-     begin 
+    begin 
       @user.save
       session[:user] = @user
       redirect_to "/members/dashboard"
-     rescue ActiveRecord::RecordNotSaved => e
-        @user.errors.full_messages << "Problem while saving records."
-        redirect_to "/members/filldetails"
-     rescue ActiveRecord::RecordNotUnique => e1
-         @user.errors.full_messages << "Username already taken."
-        redirect_to "/members/filldetails"
-     end
+    rescue ActiveRecord::RecordNotSaved => e
+      @user.errors.full_messages << "Problem while saving records."
+      redirect_to "/members/filldetails"
+    rescue ActiveRecord::RecordNotUnique => e1
+      @user.errors.full_messages << "Username already taken."
+      redirect_to "/members/filldetails"
+    end
      
   end
   
@@ -148,18 +122,18 @@ class MembersController < ApplicationController
       value["present"] ="t"
     end
     respond_to do |format|
-        format.json { render json: value }
-      end
+      format.json { render json: value }
+    end
   end
   def support
     if verify_recaptcha
-       Thread.new{SupportEmailer.contactus(params[:name],params[:email],params[:subject],params[:message])}
+      Thread.new{SupportEmailer.contactus(params[:name],params[:email],params[:subject],params[:message])}
     else
-       build_resource
-       flash[:alert] = "There was an error with the recaptcha code below. Please re-enter the code and click submit."
-       render_with_scope :contactus
-     end
-  redirect_to "/members/dashboard"
+      build_resource
+      flash[:alert] = "There was an error with the recaptcha code below. Please re-enter the code and click submit."
+      render_with_scope :contactus
+    end
+    redirect_to "/members/dashboard"
   end
   
   def unsubscribe
