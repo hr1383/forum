95f5db282d8342e7f3f7347d101515766dfac694
Harsh <hrsht.rastogi13@gmail.com>
2014-10-31 00:40:43 -0700

    fix sitemap

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -2,6 +2,8 @@ class SitemapController < ApplicationController
 	layout nil
 	respond_to :xml
 	def index
+		@host_url = "http://www.umvox.com/posts/"
+		@file_type=".html"
 		@posts = Post.all
 	end
 end
